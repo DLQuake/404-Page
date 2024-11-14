27c6eba1c172daa76054cbf22f67a746adbbf8b1
Dominik Lewczyński <dominiklew200060@gmail.com>
2024-11-14 12:09:19 +0100

    Delete function updateFontSize

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
 const slider = document.getElementById('color-slider');
 const error404 = document.getElementById('error-404');
 
-function updateFontSize() {
-    const screenWidth = window.innerWidth;
-
-    if (screenWidth > 1200) {
-        error404.style.fontSize = '25rem';
-    } else if (screenWidth > 768) {
-        error404.style.fontSize = '20rem';
-    } else {
-        error404.style.fontSize = '15rem';
-    }
-}
-
-// Funkcja do zmiany koloru "404" i aktualizacji suwaka
 slider.addEventListener('input', function () {
     const hue = slider.value;
     error404.style.color = `hsl(${hue}, 100%, 50%)`;
